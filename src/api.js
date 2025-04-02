f57852d9195f8833e8937ea322a5247d7b81e66f
msk00007 <msaikrishna.9802@gmail.com>
2025-04-02 13:08:12 +0530

    base url changed

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://62.72.29.190:8080", // Your API base URL
-  baseURL: "http://localhost:8080",
+  baseURL: "http://62.72.29.190:8080", // Your API base URL
+  // baseURL: "http://localhost:8080",
   withCredentials: true, // Ensure cookies are sent with each request
   headers: {
     "Content-Type": "application/json", // Ensure Content-Type matches the backend
